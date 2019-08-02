bb2c65d291cc4ed8efea05467adb286555b7dfef
starterDroid <30289254+Bananamilk452@users.noreply.github.com>
2019-08-03 02:25:09 +0900

    Move line ca,gnuug to top

--- travis_mw.sh
+++ travis_mw.sh
@@ -2,11 +2,11 @@
 
 echo "Install Mono and Wine.."
 sudo dpkg --add-architecture i386
+sudo apt-get install -y gnupg ca-certificates unzip
 wget -qO - https://dl.winehq.org/wine-builds/winehq.key | sudo apt-key add -
 sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ bionic main'
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 echo "deb https://download.mono-project.com/repo/ubuntu stable-bionic main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
 sudo apt update -q
 sudo apt-get install -y --no-install-recommends winehq-stable
-sudo apt-get install -y gnupg ca-certificates unzip
 sudo apt install -y mono-devel
