01a96e5616d430e26c3827b1f0840506b6e72407
starterDroid <30289254+Bananamilk452@users.noreply.github.com>
2019-08-03 15:00:25 +0900

    Update travis_mw.sh

--- travis_mw.sh
+++ travis_mw.sh
@@ -8,5 +8,5 @@ sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ bionic ma
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 echo "deb https://download.mono-project.com/repo/ubuntu stable-bionic main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
 sudo apt update -q
-sudo apt-get install -y --no-install-recommends winehq-stable
+sudo apt-get install -y --install-recommends winehq-stable
 sudo apt install -y mono-devel
